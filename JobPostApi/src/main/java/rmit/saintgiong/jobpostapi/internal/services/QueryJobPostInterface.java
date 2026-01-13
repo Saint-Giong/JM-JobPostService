1dfc1f51dfada7cc4f67e83d634d53e180b0a049
Felix101003 <dinhviet101003@gmail.com>
2026-01-13 15:21:30 +0700

    feat: add getApplicationsByJobPostId to QueryJobPostInterface
    Added method signature for retrieving applications by job post ID to support external application service integration.

--- JobPostApi/src/main/java/rmit/saintgiong/jobpostapi/internal/services/QueryJobPostInterface.java
+++ JobPostApi/src/main/java/rmit/saintgiong/jobpostapi/internal/services/QueryJobPostInterface.java
@@ -1,5 +1,6 @@
 package rmit.saintgiong.jobpostapi.internal.services;
 
+import rmit.saintgiong.jobpostapi.internal.common.dto.response.ApplicationResponseDto;
 import rmit.saintgiong.jobpostapi.internal.common.dto.response.QueryJobPostResponseDto;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface QueryJobPostInterface {
     List<QueryJobPostResponseDto> getJobPostsByCompanyId(String companyId);
 
     List<QueryJobPostResponseDto> getAllJobPosts();
+
+    List<ApplicationResponseDto> getApplicationsByJobPostId(String postId);
 }
